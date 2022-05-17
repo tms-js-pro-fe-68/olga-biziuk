d11e71940ef062dc2991ef76c2046da21cf66fe4
Olgabiziuk <biziuk_olga@mail.ru>
2022-05-17 16:44:31 +0300

    dz next

---
+++ next.js/pages/contacts/contact.js
@@ -0,0 +1,7 @@
+const Contact = () => (
+    <>
+    <h1>Jack</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, obcaecati architecto eveniet perspiciatis accusantium laudantium eligendi, modi, fugiat voluptatem cum illo praesentium voluptatibus doloribus suscipit nemo et corrupti voluptatum. Aperiam.</p>
+</>
+);
+export default Contact;

7619f52b660a3d6db6732903dd7bd0de76b20e15
Olgabiziuk <biziuk_olga@mail.ru>
2022-05-17 16:52:09 +0300

    dz next.js

---
+++ next.js/pages/posts.js
@@ -0,0 +1,7 @@
+const Posts = () => (
+    <>
+    <h1>Posts list</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, obcaecati architecto eveniet perspiciatis accusantium laudantium eligendi, modi, fugiat voluptatem cum illo praesentium voluptatibus doloribus suscipit nemo et corrupti voluptatum. Aperiam.</p>
+</>
+);
+export default Posts;
